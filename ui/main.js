04db1eff5068d14a5a2b65bf1a348c4339751450
prateekmehndiratta <mehndirattaprateek4@gmail.com>
2017-02-04 22:08:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,6 +29,6 @@ button.onclick=function(){
         var names=['name1','name2','name3'];
         var list='';
         for(var i=0;i<names.length;i++) {
-            list+= '<li>' + names[i]
+            list+= '<li>' + names[i];
         }
     };
