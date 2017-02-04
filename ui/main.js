cd173b0bfa9be1283294d93dc6e96ddd7af5233f
prateekmehndiratta <mehndirattaprateek4@gmail.com>
2017-02-04 22:00:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,4 +22,13 @@ button.onclick=function(){
     
     
     
-    
+    var nameInput = Document.getElementById('name');
+    var name=nameInput.value;
+    var submit=document.getElementById('Submit_btn');
+    submit.onclick=function() {
+        var names=['name1','name2','name3'];
+        var list='';
+        for(var i=0;i<names.length;i++) {
+            list+= '<li>' + names[i]
+        }
+    };
