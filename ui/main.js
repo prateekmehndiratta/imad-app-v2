5177c59cf2dfdeebae4e70616b278c74f0b27e1f
prateekmehndiratta <mehndirattaprateek4@gmail.com>
2017-02-04 21:31:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,24 @@
-console.log('Loaded!');
-<script>  alert("you are closing file made by great prateek mehndiratta");  </script>
+
+var button= document.getElementbyId('counter');
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function() {
+        if(request.readystate === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var counter=request.responseText;
+                var span=document.getElementbyId('count');
+                span.innerHTML = counter.tostring();
+            }
+    }
+    };
+};
+
+
+request.open('GET','http://prateekmehndiratta.imad.hasura-app.io/counter',true);
+request.send(null);
+    //make a request to counter and capture the response and store it in a varible in the correct span.
+    
+    
+    
+    
