a6039af2f4adbd6e16c9960785bdf1505b709189
prateekmehndiratta <mehndirattaprateek4@gmail.com>
2017-02-09 23:15:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,4 @@
-app.get('/ui/main.js', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+
 var button= document.getElementbyId('counter');
 button.onclick=function(){
     var request=new XMLHttpRequest();
