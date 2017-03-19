27a29e443f73b587a48bc115acd90fb84a943c4d
prateekmehndiratta <mehndirattaprateek4@gmail.com>
2017-03-19 21:41:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ button.onclick=function(){
     
     request.onreadystatechange=function() {
         if(request.readystate === XMLHttpRequest.DONE) {
-            if(request.status === 200) {
+            if(request.Status === 200) {
                 var counter=request.responseText;
                 var span=document.getElementbyId('count');
                 span.innerHTML = counter;
